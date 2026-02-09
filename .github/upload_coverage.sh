ec2a38404a4e0fbae09ca60d48624c6f54a72130
joshmn <git@josh.mn>
2026-02-09 09:22:15 -0600

    Update upload_coverage.sh

--- .github/upload_coverage.sh
+++ .github/upload_coverage.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 EVENT_PAYLOAD_FILE=$1
+PRID=$(jq ".number // .check_run.pull_requests[0].number" $EVENT_PAYLOAD_FILE)
+SHA=$(jq -r ".pull_request.head.sha // .check_run.head_sha // .after" $EVENT_PAYLOAD_FILE)
 
-PRID=`jq ".number // .check_run.pull_requests[0].number" $EVENT_PAYLOAD_FILE`
-SHA=`jq -r ".pull_request.head.sha // .check_run.head_sha // .after" $EVENT_PAYLOAD_FILE`
-
-if [ $PRID = "null" ]
-then
-  bash <(curl -s https://codecov.io/bash) -n "Caffeinate" -C $SHA
+if [ "$PRID" = "null" ]; then
+  bash <(curl -s https://codecov.io/bash) -n "Caffeinate" -C $SHA -t ${CODECOV_TOKEN}
 else
-  bash <(curl -s https://codecov.io/bash) -n "Caffeinate" -C $SHA -P $PRID
+  bash <(curl -s https://codecov.io/bash) -n "Caffeinate" -C $SHA -P $PRID -t ${CODECOV_TOKEN}
 fi
